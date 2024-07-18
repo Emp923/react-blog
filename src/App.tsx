8f179347b32068bc2458455f7185a1d9028a3f07
Eric Penrod <emp923@gmail.com>
2024-07-18 15:05:13 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="container">
+      <h1>React Blog</h1>
+      <p>Sample React Blog App using a Django backend.</p>
+    </div>
+  );
+}
+
+export default App;
